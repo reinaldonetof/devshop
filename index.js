76f4861e89245f5cc4be5aca226ff19d9b7d64fc
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-04-03 17:57:45 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+const db = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'devshop'
+  }
+})
+
+db.on('query', query => {
+  console.log('SQL: ' + query.sql)
+})
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get('/', async (req,res) => {
+  const categories = await db('categories').select('*')
+  res.render('home', {
+    categories
+  })
+})
+
+app.get('/categoria/:id', async (req, res) => {
+  const categories = await db('categories').select('*')
+  const products = await db('products').select('*').where('id', function() {
+    this.select("categories_products.product_id")
+      .from("categories_products")
+      .whereRaw("categories_products.product_id = products.id")
+      .where("category_id", req.params.id);
+  })
+  res.render('category', {
+    products,
+    categories
+  })
+})
+
+app.listen(port, err => {
+  if(err) {
+    console.log('nao foi possivel iniciador o server')
+  } else {
+    console.log('devshop rodando')
+  }
+});
