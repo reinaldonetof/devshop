61ae8e9475fc504b2ca1737cee02d0b987802cda
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-04-03 19:35:37 -0300

    refactorated index.js and begin to refactor to models

--- index.js
+++ index.js
@@ -1,66 +1,77 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const slug = require('./utils/slug')
+const slug = require("./utils/slug");
 
-const db = require('knex')({
-  client: 'mysql2',
+const db = require("knex")({
+  client: "mysql2",
   connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'devshop'
-  }
-})
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "devshop",
+  },
+});
 
-db.on('query', query => {
-  console.log('SQL: ' + query.sql)
-})
+db.on("query", (query) => {
+  console.log("SQL: " + query.sql);
+});
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get('/', async (req,res) => {
-  const categories = await db('categories').select('*')
-  const categoriesWithSlug = categories.map( category => {
-    const newCategory = { ...category, slug: slug(category.category)}
-    return newCategory
-  })
-  res.render('home', {
-    categories: categoriesWithSlug
-  })
-})
-
-app.get('/categoria/:id/:slug', async (req, res) => {
-  const categories = await db('categories').select('*')
-  
-  const products = await db('products').select('*').where('id', function() {
-    this.select("categories_products.product_id")
-      .from("categories_products")
-      .whereRaw("categories_products.product_id = products.id")
-      .where("category_id", req.params.id);
-  })
-  
-  const categoriesWithSlug = categories.map( category => {
-    const newCategory = { ...category, slug: slug(category.category)}
-    return newCategory
-  })
-  
+const getCategoryById = async (id) => {
   const category = await db("categories")
-    .select("*")
-    .where("id", req.params.id);
-  
-    res.render('category', {
-    products,
-    categories: categoriesWithSlug,
-    category
-  })
-})
+  .select("*")
+  .where("id", id);
+  return category;
+}
 
-app.listen(port, err => {
-  if(err) {
-    console.log('nao foi possivel iniciador o server')
+const getCategories = async () => {
+  const categories = await db("categories").select("*");
+  const categoriesWithSlug = categories.map((category) => {
+    const newCategory = { ...category, slug: slug(category.category) };
+    return newCategory;
+  });
+  return categoriesWithSlug;
+};
+
+const getProductsByCategoryId = async (id) => {
+  const products = await db("products")
+    .select("*")
+    .where("id", function () {
+      this.select("categories_products.product_id")
+        .from("categories_products")
+        .whereRaw("categories_products.product_id = products.id")
+        .where("category_id", id);
+    });
+  return products;
+};
+
+app.get("/", async (req, res) => {
+  const categories = await getCategories();
+  res.render("home", {
+    categories,
+  });
+});
+
+app.get("/categoria/:id/:slug", async (req, res) => {
+  const categories = await getCategories();
+  const products = await getProductsByCategoryId(req.params.id);
+  const category = await getCategoryById(req.params.id)
+
+
+  res.render("category", {
+    products,
+    categories,
+    category,
+  });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log("nao foi possivel iniciador o server");
   } else {
-    console.log('devshop rodando')
+    console.log("devshop rodando");
   }
 });
