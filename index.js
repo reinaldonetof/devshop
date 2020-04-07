72c0d9661fb4ca16f5fdab3a06a9682d8c993876
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-04-06 22:05:42 -0300

    added details page

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const category = require("./models/category");
+const product = require("./models/product");
 
 const db = require("knex")({
   client: "mysql2",
@@ -21,20 +22,10 @@ db.on("query", (query) => {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const getProductsByCategoryId = async (id) => {
-  const products = await db("products")
-    .select("*")
-    .where("id", function () {
-      this.select("categories_products.product_id")
-        .from("categories_products")
-        .whereRaw("categories_products.product_id = products.id")
-        .where("category_id", id);
-    });
-  return products;
-};
+
 
 app.get("/", async (req, res) => {
-  const categories = await category.getCategories.bind(db)();
+  const categories = await category.getCategories(db)();
   res.render("home", {
     categories,
   });
@@ -42,7 +33,7 @@ app.get("/", async (req, res) => {
 
 app.get("/categoria/:id/:slug", async (req, res) => {
   const categories = await category.getCategories(db)();
-  const products = await getProductsByCategoryId(req.params.id);
+  const products = await product.getProductsByCategoryId(db)(req.params.id);
   const categoryById = await category.getCategoryById(db)(req.params.id);
 
   res.render("category", {
@@ -51,6 +42,15 @@ app.get("/categoria/:id/:slug", async (req, res) => {
     category: categoryById,
   });
 });
+
+app.get("/produto/:id/:slug", async (req, res) => {
+  const productId = await product.getProductById(db)(req.params.id)
+  const categories = await category.getCategories(db)();
+  res.render("product-detail", {
+    product: productId,
+    categories,
+  });
+})
 
 app.listen(port, (err) => {
   if (err) {
