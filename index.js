70bd9224bf58e75aeb244df12177698b65d025b6
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-04-13 20:20:04 -0300

    devshop mvc

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
-
-const category = require("./models/category");
-
-
-const routes = require('./routes')
-
 const db = require("knex")({
   client: "mysql2",
   connection: {
@@ -21,20 +12,9 @@ db.on("query", (query) => {
   console.log("SQL: " + query.sql);
 });
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+const app = require("./app")(db);
 
-//Middleware
-app.use(async (req,res, next) => {
-  const categories = await category.getCategories(db)();
-  res.locals = {
-    categories
-  }
-  next()
-})
-
-app.use(routes(db))
-
+const port = process.env.PORT || 3000;
 
 app.listen(port, (err) => {
   if (err) {
