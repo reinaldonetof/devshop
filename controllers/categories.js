fc1828b9bb13a8ab1136b7f48535d1d43ebdfb1d
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-06-18 11:59:25 -0300

    added wrapper (init) to take off db from several times

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,71 +1,75 @@
-const category = require("../models/category");
-const product = require("../models/product");
+const init = (db) => {
+  const category = require("../models/category")(db);
+  const product = require("../models/product")(db);
 
-const getCategories = (db) => async (req, res) => {
-  const products = await product.getProductsByCategoryId(db)(req.params.id);
-  const categoryById = await category.getCategoryById(db)(req.params.id);
+  const getCategories = async (req, res) => {
+    const products = await product.getProductsByCategoryId(db)(req.params.id);
+    const categoryById = await category.getCategoryById(db)(req.params.id);
 
-  res.render("category", {
-    products,
-    category: categoryById,
-  });
-};
-
-const adminGetCategories = (db) => async (req, res) => {
-  const categories = await category.getCategories(db)();
-  res.render("admin/categories/index", {
-    categories,
-  });
-};
-
-const adminCreateCategory = (db) => async (req, res) => {
-  if (req.method === "GET") {
-    res.render("admin/categories/create", {
-      form: {},
-      errors: [],
+    res.render("category", {
+      products,
+      category: categoryById,
     });
-  } else {
-    try {
-      await category.createCateogry(db)(req.body);
-      res.redirect("/admin/categorias");
-    } catch (err) {
+  };
+
+  const adminGetCategories = async (req, res) => {
+    const categories = await category.getCategories(db)();
+    res.render("admin/categories/index", {
+      categories,
+    });
+  };
+
+  const adminCreateCategory = async (req, res) => {
+    if (req.method === "GET") {
       res.render("admin/categories/create", {
-        form: req.body,
-        errors: err.errors.fields,
+        form: {},
+        errors: [],
       });
+    } else {
+      try {
+        await category.createCateogry(db)(req.body);
+        res.redirect("/admin/categorias");
+      } catch (err) {
+        res.render("admin/categories/create", {
+          form: req.body,
+          errors: err.errors.fields,
+        });
+      }
     }
-  }
-};
+  };
 
-const adminRemoveCategory = (db) => async (req, res) => {
-  await category.removeCategory(db)(req.params.id);
-  res.redirect("/admin/categorias");
-};
+  const adminRemoveCategory = async (req, res) => {
+    await category.removeCategory(db)(req.params.id);
+    res.redirect("/admin/categorias");
+  };
 
-const adminUpdateCategory = (db) => async (req, res) => {
-  if (req.method === "GET") {
-    const cat = await category.getCategoryById(db)(req.params.id);
-    res.render("admin/categories/update", {
-      form: cat[0],
-      errors: [],
-    });
-  } else {
-    try {
-      await category.updateCategory(db)(req.params.id, req.body);
-      res.redirect("/admin/categorias");
-    } catch (err) {
+  const adminUpdateCategory = async (req, res) => {
+    if (req.method === "GET") {
+      const cat = await category.getCategoryById(db)(req.params.id);
       res.render("admin/categories/update", {
-        form: req.body,
-        errors: err.errors.fields,
+        form: cat[0],
+        errors: [],
       });
+    } else {
+      try {
+        await category.updateCategory(db)(req.params.id, req.body);
+        res.redirect("/admin/categorias");
+      } catch (err) {
+        res.render("admin/categories/update", {
+          form: req.body,
+          errors: err.errors.fields,
+        });
+      }
     }
-  }
+  };
+
+  return {
+    getCategories,
+    adminGetCategories,
+    adminCreateCategory,
+    adminRemoveCategory,
+    adminUpdateCategory,
+  };
 };
 
-module.exports = {
-  getCategories,
-  adminGetCategories,
-  adminCreateCategory,
-  adminRemoveCategory,
-  adminUpdateCategory,
-};
+module.exports = init;
--- models/category.js
+++ models/category.js
@@ -7,39 +7,44 @@ const createSchema = Joi.object({
   description: Joi.string().min(5).required(),
 });
 
-const getCategoryById = (db) => async (id) => {
-  const category = await db("categories").select("*").where("id", id);
-  return category;
+const init = (db) => {
+  const getCategoryById = async (id) => {
+    const category = await db("categories").select("*").where("id", id);
+    return category;
+  };
+
+  const getCategories = async () => {
+    const categories = await db("categories").select("*");
+    const categoriesWithSlug = categories.map((category) => {
+      const newCategory = { ...category, slug: slug(category.category) };
+      return newCategory;
+    });
+    return categoriesWithSlug;
+  };
+
+  const createCateogry = async (category) => {
+    const value = validation.validation(category, createSchema);
+    await db("categories").insert(value);
+    return true;
+  };
+
+  const updateCategory = async (id, category) => {
+    const value = validation.validation(category, createSchema);
+    await db("categories").where({ id }).update(value);
+    return true;
+  };
+
+  const removeCategory = async (id) => {
+    await db("categories").where({ id }).del();
+  };
+
+  return {
+    getCategories,
+    getCategoryById,
+    createCateogry,
+    removeCategory,
+    updateCategory,
+  };
 };
 
-const getCategories = (db) => async () => {
-  const categories = await db("categories").select("*");
-  const categoriesWithSlug = categories.map((category) => {
-    const newCategory = { ...category, slug: slug(category.category) };
-    return newCategory;
-  });
-  return categoriesWithSlug;
-};
-
-const createCateogry = (db) => async (category) => {
-  const value = validation.validation(category, createSchema);
-  await db("categories").insert(value);
-  return true;
-};
-
-const updateCategory = (db) => async (id, category) => {
-  const value = validation.validation(category, createSchema);
-  await db("categories").where({ id }).update(value);
-  return true;
-};
-
-const removeCategory = (db) => async (id) => {
-  await db("categories").where({ id }).del();
-};
-module.exports = {
-  getCategories,
-  getCategoryById,
-  createCateogry,
-  removeCategory,
-  updateCategory,
-};
+module.exports = init;
